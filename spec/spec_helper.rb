73784693c5fd1b7e262166a79ffd596ac026daef
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-07 18:53:45 +0200

    add a filter helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,48 @@ module RelpHelpers
     end
   end
 
+  def filter(events, message)
+     events.select{|event| event["message"] == message }
+  end
+
+  def input(config, size, &block)
+    pipeline = LogStash::Pipeline.new(config)
+    queue = Queue.new
+
+    pipeline.instance_eval do
+      # create closure to capture queue
+      @output_func = lambda { |event| queue << event }
+
+      # output_func is now a method, call closure
+      def output_func(event)
+        @output_func.call(event)
+      end
+    end
+
+    pipeline_thread = Thread.new { pipeline.run }
+    sleep 0.1 while !pipeline.ready?
+
+    block.call
+    sleep 0.1 while queue.size != size
+
+    result = size.times.inject([]) do |acc|
+      acc << queue.pop
+    end
+
+    pipeline.shutdown
+    pipeline_thread.join
+
+    result
+  end # def input
+
 end
 
 RSpec.configure do |c|
   c.include RelpHelpers
+end
+
+RSpec::Matchers.define :has do |size|
+  match do |pipeline|
+
+  end
 end
